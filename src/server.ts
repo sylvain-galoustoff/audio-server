3b6294c74f76e8e801fedb8a4d39b8dbc1789452
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-12-11 10:45:46 +0100

    removed mp3

--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,6 @@ app.use(
   })
 );
 
-const mp3Directory = path.join(__dirname, "..", "mp3");
-app.use("/mp3", express.static(mp3Directory));
-
 app.get("/api/categories", getCategories);
 
 const PORT = process.env.PORT || 3000;
