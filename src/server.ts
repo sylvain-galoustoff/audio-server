14f11458894f966663d9ee6fb87c56f411d4f1ee
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-12-11 09:57:30 +0100

    Add cors policy

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import { getCategories } from "./controllers/apiController";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 const mp3Directory = path.join(__dirname, "..", "mp3");
 app.use("/mp3", express.static(mp3Directory));
